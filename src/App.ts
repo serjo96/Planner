86b395a8aabe469d726a047c0f622aed2f1b66c6
Serjo96 <king-beer@yandex.ru>
2019-03-31 15:26:39 +0500

    Add global options from vuex, on init app

--- src/App.ts
+++ src/App.ts
@@ -1,13 +1,26 @@
 import Vue from 'vue'
 import Component from 'vue-class-component'
-import {  Action } from 'vuex-class';
+import * as firebase from 'firebase';
+import {  Action, Getter } from 'vuex-class';
 
 import 'vuetify/dist/vuetify.min.css';
+import store from "@/store";
 
 
 
 @Component({
 })
 export default class App extends Vue {
+
+    @Action onAuthChange: any;
+    @Action onEnablePWA: any;
+    @Getter pwaStatus!: boolean;
+
+    created(){
+        this.onAuthChange();
+        if(this.pwaStatus){
+            this.onEnablePWA();
+        }
+    }
 
 }
