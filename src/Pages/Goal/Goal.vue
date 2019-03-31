3116f026a6f52682f3fa53f982b2bdfa43f77046
Serjo96 <king-beer@yandex.ru>
2019-03-31 19:42:47 +0500

    Add goal page

--- src/Pages/Goal/Goal.vue
+++ src/Pages/Goal/Goal.vue
@@ -1,12 +1,31 @@
 <template>
-    <div v-if="loadingStatus" class="goal">
-        <div class="goal__name">{{getGoalData.name}}</div>
-    </div>
+
+    <v-jumbotron
+            height="100%"
+            :gradient="gradient"
+            dark
+            src="https://cdn.vuetifyjs.com/images/parallax/material2.jpg"
+    >
+        <v-container fill-height>
+            <v-layout align-center>
+                <v-flex text-xs-center>
+                    <div v-if="loadingStatus" class="goal">
+                        <h1 class="goal__name">{{getGoalData.name}}</h1>
+                        <div class="goal__date">
+                            <div v-if="getGoalData.createdDate" class="goal__date-created">{{getGoalData.createdDate}}</div>
+                            <div v-if="getGoalData.date" class="goal__date-deadline">{{getGoalData.date}}</div>
+                        </div>
+                        <p class="goal__description" v-if="getGoalData.description">{{getGoalData.description}}</p>
+                        <v-btn @click="onDeleteGoal">Delete</v-btn>
+                    </div>
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </v-jumbotron>
+
 </template>
 
 
 <script src="./Goal.ts" lang="ts"></script>
 
-<style lang="stylus" scoped>
-
-</style>
+<style src="./Goal.stylus" lang="stylus" scoped></style>
