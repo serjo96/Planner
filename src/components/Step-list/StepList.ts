711f102a036da72439d8331fa99762ce97bc5d1e
Serjo96 <king-beer@yandex.ru>
2019-04-01 18:51:56 +0500

    Add step list

--- src/components/Step-list/StepList.ts
+++ src/components/Step-list/StepList.ts
@@ -1,12 +1,62 @@
 import Vue from 'vue';
-import { Component } from 'vue-property-decorator';
+import { Component, Watch, Prop } from 'vue-property-decorator';
 import { Action, Getter, Mutation } from 'vuex-class';
+import { stepPayload } from "@/store/Goal/interfaces/goalInterfaces";
+import UIDatePicker from "@/components/UI/DataPicker/UIDatePicker.vue";
 
 
 @Component({
-
+    components: { UIDatePicker }
 })
 export default class StepList extends Vue {
+    stepName: string = '';
+    description: string = '';
+    date: string = '';
+    valid: boolean = false;
 
+    nameRules: any = [
+        (v: string) => !!v || 'Name is required',
+    ];
+
+    @Action addGoalStep: any;
+    @Prop(String) GoalId!: string;
+    @Prop() Steps!: stepPayload;
+
+
+    normalDate(seconds: number){
+        const date = new Date(1970, 0, 1);
+        date.setSeconds(seconds);
+        return date;
+    }
+
+    stepStatus(status: boolean){
+        return {
+            icon: status ? 'done' : '',
+            color: status ? 'success' : 'red lighten-2'
+        };
+    }
+
+
+
+
+    get formValidate(){
+        return (this.$refs.form as Vue & { validate: () => boolean }).validate()
+    }
+
+    onSubmit(){
+        const stepData = {
+            name: this.stepName,
+            description: this.description,
+            date: this.date,
+            done: false
+        };
+
+        if(this.formValidate){
+            this.addGoalStep({
+                id: this.GoalId,
+                stepData
+            });
+        }
+    }
 
 }
--- src/components/Step-list/StepList.vue
+++ src/components/Step-list/StepList.vue
@@ -1,7 +1,69 @@
 <template>
-    <div>
-        <h2>Step list</h2>
-        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi cum debitis exercitationem molestias similique temporibus unde. Delectus eligendi eum labore maiores, mollitia officia perferendis quam reiciendis saepe tempora! Expedita impedit, labore natus nobis perferendis saepe sint tempora tenetur velit! Consequatur eius laudantium nesciunt, nihil quidem repellendus sapiente sequi temporibus unde.</div>
+    <div class="goal__steps">
+        <div class="goal__steps-form">
+            <v-form
+                    ref="form"
+                    v-model="valid"
+                    @keyup.native.enter="onSubmit"
+            >
+                <div class="goal__steps-form-filed">
+                    <v-text-field
+                            solo
+                            label="Add step"
+                            v-model="stepName"
+                            :rules="nameRules"
+                    ></v-text-field>
+                </div>
+
+                <div class="goal__steps-form-filed">
+                    <v-textarea
+                            name="description"
+                            label="Description"
+                            v-model="description"
+                            auto-grow
+                            solo
+                    ></v-textarea>
+                </div>
+
+                <div class="goal__steps-form-filed">
+                    <UIDatePicker
+                            label="Deadline"
+                            v-model="date"
+                    ></UIDatePicker>
+                </div>
+                <v-btn @click="onSubmit">Add step</v-btn>
+            </v-form>
+        </div>
+
+        <div class="goal__steps-time-line">
+            <v-timeline>
+                <v-timeline-item
+                        v-for="(step, index) in Steps"
+                        :key="index"
+                        :color="stepStatus(step.done).color"
+                        large
+                >
+
+                    <template v-slot:icon>
+                        <v-btn fab small>
+                            <v-icon>{{stepStatus(step.done).icon}}</v-icon>
+                        </v-btn>
+                    </template>
+
+                    <template v-if="step.deadline" v-slot:opposite>
+                        <span>{{normalDate(step.deadline.seconds)}}</span>
+                    </template>
+
+                    <v-card class="elevation-2">
+                        <v-card-title class="step-list__step-number">{{index+1}}</v-card-title>
+                        <v-card-title class="headline">{{step.name}}</v-card-title>
+                        <v-card-text>
+                            {{step.description}}
+                        </v-card-text>
+                    </v-card>
+                </v-timeline-item>
+            </v-timeline>
+        </div>
     </div>
 </template>
 
