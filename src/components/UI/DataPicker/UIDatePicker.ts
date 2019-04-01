a4893739a5227529ff2430a6cb5c8c20ee4a5b59
Serjo96 <king-beer@yandex.ru>
2019-04-01 18:50:44 +0500

    Add ui-data-picker

---
+++ src/components/UI/DataPicker/UIDatePicker.ts
@@ -0,0 +1,49 @@
+import Vue from 'vue'
+import { Component, Watch, Prop } from 'vue-property-decorator'
+
+
+@Component({
+
+})
+export default class UIDatePicker extends Vue {
+    dateFormatted: string = '';
+    selectedDate: string = '';
+    dateValue: string = '';
+    menu: boolean = false;
+
+    @Prop(String) label!: string;
+
+    @Watch('dateFormatted')
+    ParentEmitterData(){
+        this.$emit('input', this.formattedDate)
+    }
+
+    @Watch('selectedDate')
+    onChangeDate(){
+        this.dateValue = this.selectedDate;
+    }
+
+    chooseDate(){
+        this.dateValue = this.selectedDate;
+        this.menu = false;
+    }
+
+    get formatDate () {
+        if (!this.dateValue) return '';
+        const [year, month, day] = this.dateValue.split('-');
+        return `${month}.${day}.${year}`
+    }
+
+    parseDate (date: any) {
+        if (!date) return '';
+
+        const [month, day, year] = date.split('.');
+        return `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`
+    }
+
+    get formattedDate() {
+        return this.dateValue ? this.formatDate : '';
+    }
+
+
+}
---
+++ src/components/UI/DataPicker/UIDatePicker.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-menu
+            v-model="menu"
+            :close-on-content-click="false"
+            :nudge-right="40"
+            lazy
+            transition="scale-transition"
+            offset-y
+            full-width
+            min-width="290px"
+    >
+        <template v-slot:activator="{ on }">
+            <v-text-field
+                    :value="formattedDate"
+                    :label="label"
+                    prepend-inner-icon="event"
+                    readonly
+                    v-on="on"
+                    clearable
+                    solo
+            ></v-text-field>
+        </template>
+        <v-date-picker v-model="selectedDate" scrollable>
+            <v-spacer></v-spacer>
+            <v-btn flat color="primary" @click="menu = false">Cancel</v-btn>
+            <v-btn flat color="primary" @click="chooseDate">OK</v-btn>
+        </v-date-picker>
+    </v-menu>
+</template>
+
+<script src="./UIDatePicker.ts" lang="ts"></script>
+
+<style scoped>
+
+</style>
