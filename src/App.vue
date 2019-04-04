a37b0127514ef067e2d7de53a2fd0e574a5247d8
Serjo96 <king-beer@yandex.ru>
2019-04-04 15:20:18 +0500

    Create theme stylus

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <v-app>
+    <v-app dark>
         <router-view></router-view>
     </v-app>
   </div>
@@ -8,16 +8,4 @@
 
 <script src="./App.ts" lang="ts"></script>
 
-<style>
-  @import url(https://fonts.googleapis.com/css?family=Roboto:400,300,100,500);
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  background: rgb(237, 238, 240);
-}
-    .pre-loader-parent {
-        position: relative;
-    }
-</style>
+<style src="./themes/index.stylus" lang="stylus"></style>
