494fcb3dd9e2cd7036a43c35792af08b393b5423
Serjo96 <king-beer@yandex.ru>
2019-04-08 02:55:42 +0500

    Rename user module

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Vuex,{ Store } from 'vuex';
 import {State} from './state';
-import UserModule from "./User";
+import UserModule from "./User/User";
 import Auth from "./Auth/Auth";
 import GoalForm from "@/store/GoalForm/GoalForm";
 import GoalList from "@/store/GoalList/GoalList";
