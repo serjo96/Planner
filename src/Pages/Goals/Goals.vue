752cf5157c68a5658194cfdcc9e1bfd8369098f0
Serjo96 <king-beer@yandex.ru>
2019-04-08 02:55:58 +0500

    Redesign goals list

--- src/Pages/Goals/Goals.vue
+++ src/Pages/Goals/Goals.vue
@@ -1,54 +1,84 @@
 <template>
-    <div class="goals">
-        <h1 class="page-title">Goals</h1>
+    <div class="page-content">
+        <div class="goals">
+            <h1 class="page-title">Goals</h1>
 
-        <div class="goals__list">
-            <transition-group name="goals__list-animation">
+            <div class="goals__list">
+                <transition-group name="goals__list-animation">
 
-            <div
-                    v-for="(item, index) in goalsData"
-                    :key="index"
-                    class="goals__list-item">
+                <div
+                        v-for="item in goalsData"
+                        :key="item.name"
+                        class="goals__list-item">
 
-                    <v-layout row wrap>
+                        <v-layout row wrap>
 
-                        <v-flex xs6>
-                            <router-link
-                                    class="goals__list-item-link"
-                                    :to="`/goal-${item.id}`"
-                            >
-                                <v-list-tile-title v-html="item.name"></v-list-tile-title>
-                                <v-list-tile-sub-title v-html="item.description"></v-list-tile-sub-title>
-                             </router-link>
-                        </v-flex>
+                            <v-flex xs6>
+                                <router-link
+                                        class="goals__list-item-link"
+                                        :to="`/goal-${item.id}`"
+                                >
+                                    <v-list-tile-title class="goals__list-item-title" v-html="item.name"></v-list-tile-title>
+                                    <v-list-tile-sub-title class="goals__list-item-description" v-html="item.description"></v-list-tile-sub-title>
+                                 </router-link>
+                            </v-flex>
 
-                        <v-flex xs4>
-                            <router-link
-                                    class="goals__list-item-link"
-                                    :to="`/goal-${item.id}`"
-                            >
-                                <v-list-tile-action-text>{{ item.date }}</v-list-tile-action-text>
-                            </router-link>
-                        </v-flex>
+                            <v-flex xs5>
+                                <router-link
+                                        class="goals__list-item-link"
+                                        :to="`/goal-${item.id}`"
+                                >
+                                    <v-list-tile-action-text>{{ item.date }}</v-list-tile-action-text>
+                                </router-link>
+                            </v-flex>
 
-                        <v-flex text-xs-right xs2>
-                            <v-btn @click="deleteGoalFromList(item.id)"
-                                   icon
-                                   ripple
-                                   fab
-                                   dark
-                                   small
-                                   color="red"
-                            >
-                                <v-icon>delete</v-icon>
-                            </v-btn>
-                        </v-flex>
-                    </v-layout>
-                </div>
-            </transition-group>
+                            <v-flex text-xs-right xs1>
+                                <v-menu class="drop-down-menu" bottom light left>
+                                    <template v-slot:activator="{ on }">
+                                        <v-btn
+                                                dark
+                                                icon
+                                                v-on="on">
+                                            <v-icon>more_vert</v-icon>
+                                        </v-btn>
+                                    </template>
+
+                                    <v-list>
+                                        <v-list-tile>
+
+                                                <v-icon size="19" >edit</v-icon>
+                                                <v-list-tile-title class="drop-down-menu__text">Edit</v-list-tile-title>
+
+                                        </v-list-tile>
+
+                                        <v-list-tile color="red" @click="deleteGoalFromList(item.id)">
+
+                                                <v-icon size="19" color="red">delete</v-icon>
+                                                <v-list-tile-title class="drop-down-menu__text">Delete</v-list-tile-title>
+
+                                        </v-list-tile>
+                                    </v-list>
+                                </v-menu>
+
+
+                                <!--<v-btn @click="deleteGoalFromList(item.id)"-->
+                                       <!--icon-->
+                                       <!--ripple-->
+                                       <!--fab-->
+                                       <!--dark-->
+                                       <!--small-->
+                                       <!--color="red"-->
+                                <!--&gt;-->
+                                    <!--<v-icon>delete</v-icon>-->
+                                <!--</v-btn>-->
+                            </v-flex>
+                        </v-layout>
+                    </div>
+                </transition-group>
+            </div>
+
+            <PreLoader v-if="!requestStatus"></PreLoader>
         </div>
-
-        <PreLoader v-if="!requestStatus"></PreLoader>
     </div>
 </template>
 
