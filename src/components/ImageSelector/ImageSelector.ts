d5d7c5ad03c671d46398cd98debb0331f2b747bb
Serjo96 <king-beer@yandex.ru>
2019-04-09 23:53:28 +0500

    Add image uploader dialog

---
+++ src/components/ImageSelector/ImageSelector.ts
@@ -0,0 +1,56 @@
+import Vue from 'vue'
+import { Component, Prop } from 'vue-property-decorator'
+import { Action, Getter } from 'vuex-class';
+import PreLoader from "@/components/Preloader/PreLoader.vue";
+
+
+
+@Component({
+    components: {PreLoader}
+})
+export default class ImageSelector extends Vue {
+    file: any = {};
+
+
+    @Getter('userData') User: any;
+    @Getter getUploadStatus!: boolean;
+    @Getter getStartUploading!: boolean;
+    @Action uploadImage: any;
+    @Action startUploading: any;
+    @Prop(Boolean) value!: boolean;
+
+    get dialogModal(){
+        return this.value;
+    }
+
+    set dialogModal(dialog: boolean){
+        this.$emit('input', dialog)
+    }
+
+
+    uploadFile(){
+        const reader = new FileReader();
+        const input = <HTMLInputElement> this.$refs.uploaderInput;
+        this.file = input!.files;
+        this.uploadImage(this.file[0]);
+        // reader.onload = () => {
+        //         const img = new Image();
+        //         img.onload = ()=> {
+        //             this.drawCanvasImage(img)
+        //         };
+        //         img.src = URL.createObjectURL(this.file[0]);
+        //     };
+        // reader.readAsDataURL(this.file[0]);
+
+    }
+
+    drawCanvasImage(img: HTMLImageElement) {
+        const canvas =  <HTMLCanvasElement> this.$refs.canvasImage;
+
+
+        let ctx = canvas.getContext('2d');
+        ctx!.drawImage(img,0,0);
+    }
+
+
+}
---
+++ src/components/ImageSelector/ImageSelector.vue
@@ -0,0 +1,59 @@
+<template>
+    <v-dialog
+            v-model="dialogModal"
+            width="500"
+            dark
+            class="image-selector"
+    >
+        <v-form
+                @keyup.native.enter="onSubmit"
+        >
+
+            <v-card>
+                <v-card-title class="image-selector__header">
+                    <span class="headline display-1">Change photo</span>
+                    <v-btn @click="dialogModal = false" icon>
+                        <v-icon>close</v-icon>
+                    </v-btn>
+                </v-card-title>
+
+                <div class="image-selector__body text-xs-center">
+                    <div class="image-selector__selector-heading">
+                        <p>Update your profile photo</p>
+                    </div>
+
+
+                    <div  class="image-selector__selector">
+                        <PreLoader v-if="getStartUploading && !getUploadStatus"></PreLoader>
+                        <canvas v-if="getUploadStatus" class="image-selector__selector-canvas" ref="canvasImage"></canvas>
+                    </div>
+                </div>
+
+                <div class="image-selector__footer">
+                    <input accept="image/*"
+                           id="upload-image"
+                           class="image-selector__upload-input"
+                           type="file"
+                           ref="uploaderInput"
+                           @change="uploadFile">
+
+                    <v-btn
+                            :loading="getStartUploading && !getUploadStatus"
+                            class="image-selector__upload-button">
+                        <label for="upload-image">
+                            <span>upload photo</span>
+                            <v-icon>cloud_upload</v-icon>
+                        </label>
+                    </v-btn>
+                </div>
+
+
+            </v-card>
+        </v-form>
+
+    </v-dialog>
+</template>
+
+<script lang="ts" src="./ImageSelector.ts"></script>
+
+<style src="./Image-selector.stylus" lang="stylus" scoped></style>
