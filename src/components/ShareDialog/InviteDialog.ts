4beec644bd60b3d7928c509e4fb32b81ccd2b1d7
Serjo96 <king-beer@yandex.ru>
2019-05-01 01:55:55 +0500

    Add invite dialog

---
+++ src/components/ShareDialog/InviteDialog.ts
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import { Component, Prop, Watch } from 'vue-property-decorator'
+import { Action, Getter } from 'vuex-class';
+
+
+
+@Component({
+})
+export default class InviteDialog extends Vue {
+    inviteLink: boolean = false;
+    inviteURL: string = '';
+    iviteEmail: string = '';
+
+
+
+
+    onClickCreateLink(){
+        this.inviteLink = !this.inviteLink;
+    }
+
+    selectInviteInput(){
+        const input = <any> this.$refs.inviteInput;
+        this.inviteURL = document.URL;
+        setTimeout(()=> input.$refs.input.select(),1)
+
+    }
+
+    onClickCopyLink(){
+        document.execCommand('copy');
+    }
+
+}
---
+++ src/components/ShareDialog/InviteDialog.vue
@@ -0,0 +1,70 @@
+<template>
+    <v-menu
+            min-width="334"
+            :close-on-content-click="false"
+            offset-y>
+        <template v-slot:activator="{ on }">
+            <v-btn
+                    color="primary"
+                    dark
+                    v-on="on"
+            >
+                Share
+            </v-btn>
+        </template>
+
+        <v-card>
+
+            <v-card-title>
+                <h2 class="headline">Invite peoples</h2>
+            </v-card-title>
+
+            <v-container grid-list-md>
+                <div class="invite-link-form">
+                    <v-text-field
+                            solo-inverted
+                            label="email"
+                            v-model="iviteEmail"
+                    ></v-text-field>
+
+                    <v-btn block>Send invite</v-btn>
+                </div>
+
+                <div class="invite-link-actions">
+                    <div class="invite-link-actions__title font-weight-medium grey--text darken-2--text">Invite by link</div>
+                    <div class="invite-link-actions__text font-weight-light">Anyone with a link can join as member.</div>
+
+                    <v-btn class="mx-0 blue-grey lighten-1"
+                           small
+                           @click="onClickCreateLink"
+                    >{{inviteLink ? 'Disable' : 'Create'}} link</v-btn>
+
+                    <div class="invite-link-actions__invite-link"
+                        v-if="inviteLink"
+                    >
+                        <v-text-field
+                                autofocus
+                                ref="inviteInput"
+                                readonly
+                                :value="inviteURL"
+                                @focus="selectInviteInput"
+                        >
+                        </v-text-field>
+                        <v-btn
+                                @click="onClickCopyLink"
+                                small
+                                class="my-0">
+                            Copy</v-btn>
+                    </div>
+                </div>
+
+            </v-container>
+
+
+        </v-card>
+    </v-menu>
+</template>
+
+<script src="./InviteDialog.ts" lang="ts"></script>
+
+<style src="./inviteMenu.stylus" lang="stylus" scoped></style>
