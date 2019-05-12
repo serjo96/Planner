3dca95a8e735ebd71e794dcc7090f72b8b7482b0
Serjo96 <king-beer@yandex.ru>
2019-05-13 00:49:36 +0500

    Add inviting by email function

--- src/store/InviteUser/InviteUser.ts
+++ src/store/InviteUser/InviteUser.ts
@@ -6,13 +6,16 @@ import * as firebase from 'firebase';
 export default class InviteUser extends VuexModule {
     @Action({rawError: true})
     createInviteUser(email: string) {
-        let inviteFunction = firebase.functions().httpsCallable('helloWorld');
+        let inviteFunction = firebase.functions().httpsCallable('createInviteUser');
 
         inviteFunction(email)
             .then((result) => {
             // Read result of the Cloud Function.
            console.log(result);
-        });
+        })
+            .catch(err=>{
+                console.log(err)
+            });
     }
 
 }
